93c809e69b1f198a992680a20b884a960a0aeaee
Andrey Belov <andrey.a.belov@sbermegamarket.ru>
2024-05-28 17:27:37 +0300

    Kafka yaml bug fixes

--- Makefile
+++ Makefile
@@ -1,11 +1,14 @@
+include .env
+export
+
 .PHONY: run_watch_monitoring_tempo
-run_watch:
+run_watch_monitoring_tempo:
 	docker compose -f dc-prom-grafana-tempo.yaml watch
 
 .PHONY: run_with_kafka_watch
 run_with_kafka_watch:
 	docker compose -f docker-compose-kafka.yaml -f dc-prom-grafana-tempo.yaml watch
 
-.PHONY: run_with_kafka_watch
-run_with_kafka_watch:
-	docker compose -f docker-compose-kafka.yaml -f dc-prom-grafana-tempo.yaml watch
+.PHONY: run_with_kafka_sasl-scram
+run_with_kafka_sasl:
+	docker compose -f dc-kafka-sasl-scram.yaml up -d
---
+++ go_examples/kafka/sasl_scram/main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"log"
+
+	"github.com/segmentio/kafka-go"
+	"github.com/segmentio/kafka-go/sasl/scram"
+)
+
+const (
+	topic    = "test-topic"      // replace with your topic
+	broker   = "localhost:29092" // replace with your broker
+	username = "admin"           // replace with your username
+	password = "admin-secret"    // replace with your password
+)
+
+func main() {
+
+	mechanism, err := scram.Mechanism(scram.SHA512, username, password)
+	if err != nil {
+		log.Panicln(err)
+		return
+	}
+
+	writer := &kafka.Writer{
+		Addr:         kafka.TCP(broker),
+		Topic:        topic,
+		RequiredAcks: kafka.RequireOne,
+		BatchTimeout: 100 * time.Millisecond,
+		BatchSize:    1000,
+		Balancer:     &kafka.LeastBytes{},
+		Transport: &kafka.Transport{
+			SASL: mechanism,
+		},
+	}
+
+	dialer := &kafka.Dialer{
+		DualStack:     true,
+		SASLMechanism: mechanism,
+	}
+
+	reader := kafka.NewReader(kafka.ReaderConfig{
+		Brokers:         []string{broker}, // replace with your broker
+		Topic:           topic,            // replace with your topic
+		Dialer:          dialer,
+		ReadLagInterval: 0,
+		Partition:       0,
+		MinBytes:        10e3, // 10KB
+		MaxBytes:        10e6, // 10MB
+	})
+
+	defer reader.Close()
+
+	log.Printf("Producing message into topic: %v", topic)
+
+	var msgs []kafka.Message
+
+	for i := 0; i < 100; i++ {
+		msgs = append(
+			msgs,
+			kafka.Message{
+				Key:   []byte("Key"),
+				Value: []byte(fmt.Sprintf("Hello World number %d!", i)),
+			},
+		)
+	}
+
+	err = writer.WriteMessages(context.Background(), msgs...)
+	if err != nil {
+		log.Fatal("failed to write messages:", err)
+	}
+
+	writer.Close()
+
+	log.Printf("Comsuming message from topic: %v", topic)
+	for {
+		m, err := reader.ReadMessage(context.Background())
+		if err != nil {
+			fmt.Printf("could not read message: %v\n", err)
+		}
+
+		log.Printf("message received: %s = %s\n", string(m.Key), string(m.Value))
+	}
+}
